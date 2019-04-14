e25feaba77eadca0aca0c7d1942e678948ee464f
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-04-14 21:14:07 +0900

    building user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    unless user == current_user || user.admin
+      redirect_to root_url
+    end
+  end
+
+  def admin_user
+    user = User.find(params[:id])
+    unless user.admin
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'statics/index'
-
   root to: 'statics#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users
 end
